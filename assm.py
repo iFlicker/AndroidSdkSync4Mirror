f1f7a125ef8575bca9317e3d156c5bab090ba713
iFlicker <ffflicker@gmail.com>
2016-04-23 00:29:53 +0800

    增加处理配置文件

--- assm.py
+++ assm.py
@@ -3,11 +3,17 @@
 import getopt
 import sys
 from loger import Logger
+import json
+
+global syncdir   #更新地址
+global logdir    #日志存放地址
+global checktime #检查时间间隔
+global upstream  #上游源地址
 
 crlog = Logger()
 
+# 程序入口,参数操作
 option,args = getopt.getopt(sys.argv[1:],"Vhs:",["version","help","start","stop","status"])
-
 for key,value in option:
     if key in ("-V","--version"):
         print "current version" #待定
@@ -25,10 +31,28 @@ for key,value in option:
             break
     if key in ("--start"):
         print "start task"      #待定  value为配置脚本路径
-        crlog.info("啦啦")
-        crlog.info("fdssfdfdsfs")
-        crlog.debug("sdssssssssssssssssssssssss")
+        #crlog.info("啦啦")
+        #crlog.info("fdssfdfdsfs")    #日志测试
+        #crlog.debug("sdssssssssssssssssssssssss")
     if key in ("--stop"):
         print "stop task"       #待定
     if key in ("--status"):
         print "return current status"      #待定
+
+
+# 检测并加载配置文件
+def pconfig():
+    global syncdir
+    global logdir
+    global checktime
+    global upstream
+    filejs = open("assm.conf", "r")
+    decoded = json.loads(filejs.read())
+    filejs.close()
+    syncdir = decoded["syncdir"]
+    logdir = decoded["logdir"]
+    checktime = decoded["checktime"]
+    upstream = decoded["upstream"]
+
+pconfig()
+
